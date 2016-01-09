ddbff0dd3d07c5d863fc1902909983d13ae07101
Pascal Friederich <paukul@gmail.com>
2016-01-09 14:19:49 -0800

    reload module

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express')
+  , bodyParser = require('body-parser')
+  , reload = require('reload')
+  , http = require('http')
+
+
 var app = express();
 var state = {sat: 254, bri: 64, hue: 25500};
 
+app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
-app.set('port', (process.env.PORT || 5000));
 
 app.put('/api/342716561e24f19024c9edfb8f89eee/lights/1/state', function (req, res) {
   console.log(req.body);
@@ -20,6 +24,10 @@ app.get('/', function (req, res) {
   res.send(state);
 });
 
-app.listen(app.get('port'), function () {
+var server = http.createServer(app);
+reload(server, app);
+
+server.listen(app.get('port'), function () {
   console.log("listening on " + app.get('port'));
 });
+
